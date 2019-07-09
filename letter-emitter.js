b9cab3aa3b03243cb2255353c64fa98a5caa0f9d
Jackmc <jackmc1122@gmail.com>
2019-07-09 16:14:35 -0700

    Add more events if one of the letters in the array from the constructor is included in the string from read.

--- letter-emitter.js
+++ letter-emitter.js
@@ -1,9 +1,15 @@
 const EventEmitter = require('events');
 
 class LetterEmitter extends EventEmitter {
+  constructor(arr) {
+    super();
+    this.arr = arr;
+  }
+
   read(str) {
     str.split('').forEach((letter, i) => {
       this.emit('letter', { letter, offset: i });
+      if(this.arr && this.arr.includes(letter)) this.emit(letter.toString);
     });
 
     this.emit('end');
--- letter-emitter.test.js
+++ letter-emitter.test.js
@@ -1,11 +1,40 @@
 const LetterEmitter = require('./letter-emitter');
 
-describe('Letter Emitter', () => {
-  it('emits a letter even for each letter/space in a string', done => {
+describe('Letter Emitter constructor', () => {
+
+  let le;
+  beforeEach(() => {
+    le = new LetterEmitter(['a', 'b']);
+  });
+
+  it('emits an event for each letter in an array passed to the constructor', done => {
+    le.on('a', () => console.log('a found'));
+    le.on('b', () => console.log('b found'));
+    le.on('end', () => {
+      console.log('done');
+      done();
+    });
+
+    le.read('abracadabra');
+  });
+});
+
+describe('Letter Emitter read method', () => {
+
+  let letterEmitter;
+  beforeEach(() => {
+    letterEmitter = new LetterEmitter();
+  });
+
+  it('emits a letter event for each letter/space in a string', done => {
     const str = 'Hello my name is Jack';
-    const letterEmitter = new LetterEmitter();
+
 
     const mockFunction = jest.fn();
+
+    letterEmitter.on('test', () => {
+      console.log('constructor is working');
+    });
 
     letterEmitter.on('letter', data => {
       mockFunction();
